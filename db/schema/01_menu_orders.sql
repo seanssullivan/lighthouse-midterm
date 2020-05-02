40bbbe3a123ef01af2d5f7d47c2da1177c841ba8
Sean Sullivan <dev@seanmedia.ca>
2020-05-02 01:25:32 +0000

    Write SQL for orders table

--- db/schema/01_menu_orders.sql
+++ db/schema/01_menu_orders.sql
@@ -1,6 +1,7 @@
 -- Drop and recreate Users table (Example)
 
 DROP TABLE IF EXISTS menu_items CASCADE;
+DROP TABLE IF EXISTS orders CASCADE;
 
 CREATE TABLE menu_items (
   id SERIAL PRIMARY KEY NOT NULL,
@@ -9,4 +10,17 @@ CREATE TABLE menu_items (
   cost REAL NOT NULL,
   image_url VARCHAR(255) NOT NULL,
   sold_out BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE orders (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  phone VARCHAR(16) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  ordered_at DATE,
+  confirmed_at DATE,
+  estimated_time DATE,
+  ready_at DATE,
+  completed_at DATE,
+  notes TEXT
 );
