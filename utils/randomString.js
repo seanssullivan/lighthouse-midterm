01e5cc5e52d60ec5635bfb30ebf5758572f47b28
Sean Sullivan <dev@seanmedia.ca>
2020-05-04 01:37:49 +0000

    Create visitor middleware to handle visitor cookies

---
+++ utils/randomString.js
@@ -0,0 +1,19 @@
+// randomString.js
+
+/**
+ * Generates a unique alphanumeric id of a given length.
+ * @param {number} length
+ */
+const generateRandomString = (length) => {
+  const alphaChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let randomString = '';
+  for (let i = 0; i < length; i++) {
+    const index = Math.floor(Math.random() * alphaChars.length);
+    randomString += alphaChars[index];
+  }
+  return randomString;
+};
+
+module.exports = {
+  generateRandomString
+};
