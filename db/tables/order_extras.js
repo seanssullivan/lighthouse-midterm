6c43bbf02834632298a862fe1681c4fa09f01adf
Sean Sullivan <dev@seanmedia.ca>
2020-05-04 03:04:31 +0000

    Add order extras class object for handling all interactions with database table

---
+++ db/tables/order_extras.js
@@ -0,0 +1,14 @@
+// db/tables/order_extras.js
+
+/**
+ * Manages all requests to the order_extras table of the database.
+ */
+class OrderExtrasTable {
+
+  constructor(database) {
+    this.db = database;
+  }
+
+}
+
+module.exports = ItemReviewsTable;
