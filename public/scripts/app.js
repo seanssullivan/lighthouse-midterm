51fca3a93a0b048c784f30eb6365ba825cffe416
heisey <jheisler01@gmail.com>
2020-05-13 20:34:29 +0000

    merge change

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -247,11 +247,11 @@ $(document).ready(function() {
           if (data.pickUpTime) {
             const pickUpTime = new Date(data.pickUpTime);
             const minutes = Math.round((pickUpTime.getTime() - Date.now()) / 1000 / 60);
-            $('#pending-block').hide();
-            $('#confirmed-block').show();
-            $('#pickUpTime').text(`${minutes}`);
-            $('#pending-bell').hide();
-            $('#confirmed-bell').show();
+            $('.checkoutInfo__pending').addClass('hide');
+            $('.checkoutInfo__confirmed').removeClass('hide');
+            // $('#pickUpTime').text(`${minutes}`);
+            // $('#pending-bell').hide();
+            // $('#confirmed-bell').show();
 
             clearInterval(intervalId);
           }
@@ -259,7 +259,7 @@ $(document).ready(function() {
     }, 5000);
   }
 
-  const submitPending = function() {
+  const submitPending = function(data) {
     $('.checkoutInfo__text').addClass('hide')
     $('.checkoutInfo__pending').removeClass('hide')
 
